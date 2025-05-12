304a900e31bdc2308d99bf629f82581b312454f8
Hemanth <hemanth-kumar.tulabandula.211@my.csun.edu>
2025-05-12 02:39:56 -0700

    Switch backend database to PostgreSQL

--- obj/Debug/net8.0/InventoryAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/InventoryAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31cc29ad0a1f23152058080b003c65a5a72ec8e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+315f47dc858ef83051d08e0072e133ddff937fa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
